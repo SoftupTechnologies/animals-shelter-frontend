a741be61fe8b4419255765588642aaea224ebf0f
Dionis <dioniuliu@gmail.com>
2023-01-11 16:33:26 +0100

    Finished Login UI & React Hook Form & Not Found

--- src/components/loginPage/index.tsx
+++ src/components/loginPage/index.tsx
@@ -1,6 +1,104 @@
+import React from 'react';
+import { FaLock, FaUser } from 'react-icons/fa';
+import { BsFillXCircleFill } from 'react-icons/bs';
+import { Button, Input } from 'antd';
+import { useForm, Controller } from 'react-hook-form';
+import { loginTypes } from './types';
+import { loginSchema } from './validation';
+import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './index.module.scss';
 
 const Login = () => {
-  return <h1 className={styles.test}>hello</h1>;
+  const { control, handleSubmit } = useForm<loginTypes>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(loginSchema),
+  });
+  const onSubmit = handleSubmit((values) => {
+    console.log(values);
+  });
+
+  return (
+    <div className={styles.loginWrapper}>
+      <img
+        className={styles.loginLogo}
+        src="/images/animal_shelter.png"
+        alt="logo-shelter"
+      />
+      <h1 className={styles.loginTitle}>Animals Shelter</h1>
+
+      <div className={styles.loginForm}>
+        <Controller
+          control={control}
+          name="email"
+          render={({
+            field: { onChange, onBlur, value, name },
+            fieldState: { error },
+          }) => (
+            <span>
+              <Input
+                prefix={<FaUser style={{ color: '#7888af' }} />}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+                status={error ? 'error' : ''}
+                suffix={
+                  error ? (
+                    <BsFillXCircleFill className={styles.errorStyle} />
+                  ) : (
+                    ''
+                  )
+                }
+                name={name}
+                placeholder="animal@example.com"
+                size="large"
+              />
+              <span className={styles.errorStyle}>{error?.message}</span>
+            </span>
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          render={({
+            field: { onChange, onBlur, value, name, ref },
+            fieldState: { error },
+          }) => (
+            <span>
+              <Input
+                prefix={<FaLock style={{ color: '#7888af' }} />}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+                status={error ? 'error' : ''}
+                suffix={
+                  error ? (
+                    <BsFillXCircleFill className={styles.errorStyle} />
+                  ) : (
+                    ''
+                  )
+                }
+                name={name}
+                type="password"
+                size="large"
+                placeholder="Password"
+              />
+              <span className={styles.errorStyle}>{error?.message}</span>
+            </span>
+          )}
+        />
+        <Button
+          onClick={onSubmit}
+          type="primary"
+          size="large"
+          className={styles.loginFormButton}
+        >
+          Log in
+        </Button>
+      </div>
+    </div>
+  );
 };
 export default Login;
---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export default {
+  APP: '/app/dashboard',
+  LOG_IN: '/login',
+  LOG_OUT: '/logout',
+  NOT_FOUND: 'not_found',
+};
