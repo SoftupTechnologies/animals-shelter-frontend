e2ccc3d5c0bb9cd185a792e3e93c6204dcde2e12
Enri Rane <enrirane@gmail.com>
2023-02-06 15:37:45 +0100

    Finished the file architecture

---
+++ src/pages/MainPage/features/DataTable/types.ts
@@ -0,0 +1,25 @@
+export interface DataType {
+  key: React.Key;
+  breed: string;
+  origin: string;
+  gender: string;
+  age: number;
+  chipped: string;
+  inShelter: string;
+  isAlive: string;
+}
+
+export interface ExpandedDataType {
+  key: React.Key;
+  chip_number: string;
+  parvo_vaccine: string;
+  chip_date: string;
+  chip_position: string;
+  death_date: string;
+  death_cause: string;
+  images: string;
+}
+
+export type newDataPropsType = {
+  showDataModal: (a: boolean) => void;
+};
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from 'antd';
+import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../constants/routes';
+import styles from './index.module.scss';
+
+const NotFound = (): ReactElement => {
+  return (
+    <div className={styles.notFoundWrapper}>
+      <img
+        className={styles.notFoundLogo}
+        src="/images/dog.png"
+        alt="logo-shelter"
+      />
+      <h1 className={styles.notFoundTitle}>I'm lost! Can't find the page.</h1>
+      <Button type="primary" size="large" className={styles.notFoundButton}>
+        <Link to={routes.APP_DASHBOARD} className={styles.notFoundButtonLink}>
+          Go to main page
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default NotFound;
